2c94131873d0afe2998f785946f579d0480d60da
zondahuman <zondahuman@gmail.com>
2018-09-14 11:00:52 +0800

    add

--- dubbo-rpc1-consumer/src/main/java/com/abin/lee/dubbo/rpc/consumer/filter/RpcLogSessionFilter.java
+++ dubbo-rpc1-consumer/src/main/java/com/abin/lee/dubbo/rpc/consumer/filter/RpcLogSessionFilter.java
@@ -2,6 +2,7 @@ package com.abin.lee.dubbo.rpc.consumer.filter;
 
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.common.TraceIdUtil;
 import org.apache.dubbo.rpc.*;
 
 import java.util.UUID;
@@ -13,7 +14,8 @@ public class RpcLogSessionFilter implements Filter {
 
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String sessionId = UUID.randomUUID().toString();
-        String traceId = RpcContext.getContext().getAttachment("traceId");
+//        String traceId = RpcContext.getContext().getAttachment("traceId");
+        String traceId = TraceIdUtil.getTraceId();
         if (StringUtils.isNotBlank(traceId)) {
             System.out.println("accept traceId=" + traceId);
         } else {
--- dubbo-rpc1-provider/src/main/java/com/abin/lee/dubbo/rpc/provider/filter/ProviderRpcLogSessionFilter.java
+++ dubbo-rpc1-provider/src/main/java/com/abin/lee/dubbo/rpc/provider/filter/ProviderRpcLogSessionFilter.java
@@ -1,6 +1,7 @@
 package com.abin.lee.dubbo.rpc.provider.filter;
 
 
+import org.apache.dubbo.common.TraceIdUtil;
 import org.apache.dubbo.rpc.*;
 
 /**
@@ -10,6 +11,7 @@ public class ProviderRpcLogSessionFilter implements Filter {
 
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String traceId = RpcContext.getContext().getAttachment("traceId");
+//        String traceId = TraceIdUtil.getTraceId();
         System.out.println("ProviderRpcLogSessionFilter accept traceId=" + traceId);
         //服务段处理完了,给客户端回传回去之前他自己传过来的traceId
 //        RpcContext.getContext().setAttachment("traceId", traceId);
