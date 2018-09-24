e18699fa0df4083a48d8b146ae680aff6aed1875
zondahuman <zondahuman@gmail.com>
2018-09-24 21:39:40 +0800

    add

--- dubbo-rpc1-provider/src/main/java/com/abin/lee/dubbo/rpc/provider/filter/ProviderRpcLogSessionFilter.java
+++ dubbo-rpc1-provider/src/main/java/com/abin/lee/dubbo/rpc/provider/filter/ProviderRpcLogSessionFilter.java
@@ -11,7 +11,7 @@ public class ProviderRpcLogSessionFilter implements Filter {
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String traceId = RpcContext.getContext().getAttachment("traceId");
 //        String traceId = TraceIdUtil.getTraceId();
-        System.out.println("ProviderRpcLogSessionFilter accept traceId=" + traceId);
+//        System.out.println("ProviderRpcLogSessionFilter accept traceId=" + traceId);
         //服务段处理完了,给客户端回传回去之前他自己传过来的traceId
 //        RpcContext.getContext().setAttachment("traceId", traceId);
         return invoker.invoke(invocation);
